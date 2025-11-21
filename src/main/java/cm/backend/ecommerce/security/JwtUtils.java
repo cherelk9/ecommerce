c916e52b6e1f5bb7699ff8b9e92ba54f7b8e1049
cherelk9 <cherel.ongouda@gmail.com>
2025-11-21 17:42:58 +0100

    "implementation de jwtUtils "

---
+++ src/main/java/cm/backend/ecommerce/security/JwtUtils.java
@@ -0,0 +1,85 @@
+package cm.backend.ecommerce.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtils {
+
+    private final SecretKey key;
+    private final long expirationMs;
+
+    public JwtUtils(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expirationMs}") long expirationMs) {
+
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expirationMs = expirationMs;
+
+    }
+
+    public String generateToken(Authentication authentication) {
+
+        var username = authentication.getName();
+
+        String authorities = authentication.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", authorities)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean valideToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key) // key doit être un java.security.Key
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public String getRole(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token).getBody();
+
+        return claims.get(token, String.class);
+    }
+
+}
